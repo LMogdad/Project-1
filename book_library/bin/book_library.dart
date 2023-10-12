e0ef532344f6157ccdb3445c95ca0c528cfff368
LMogdad <Zezoodj1200@gmail.com>
2023-10-12 23:38:12 +0300

    adding basics to main

--- book_library/bin/book_library.dart
+++
@@ -1,5 +0,0 @@
-import 'package:book_library/book_library.dart' as book_library;
-
-void main(List<String> arguments) {
-  print('Hello world: ${book_library.calculate()}!');
-}
---
+++ book_library/bin/main.dart
@@ -0,0 +1,60 @@
+import 'package:book_library/book_library.dart' as book_library;
+import 'dart:io';
+void main(List<String> arguments) {
+  bool isTerminated = false;
+  while(!isTerminated){
+    print("Please select an option from the menu below by entering the corresponding number:\n1.Query Books: \n2.Add New Book: \n3.Delete Book: \n4.Purchase: \n5.View Invoice: \n6.Edit Book Information: \n7.Display All Book Categories: \n8.Reporting: \n9.Terminate");
+    int? userInput;
+    
+    try {
+      userInput = int.parse(stdin.readLineSync()!);
+    } catch(error) {
+      //For an incorrect input type, userInput will be set to 0, triggering the default case.
+      userInput = 0;
+    }
+    
+    switch(userInput){
+      //search
+      case 1: {
+      }
+      break;
+      //add book
+      case 2: {
+      }
+      break;
+      //delete book
+      case 3: {
+      }
+      break;
+      //Purchase
+      case 4: {
+      }
+      break;
+      //Invoice
+      case 5: {
+      }
+      break;
+      //Editing
+      case 6: {
+      }
+      break;
+      //Display all Book Categories
+      case 7: {
+      }
+      break;
+      //Reporting
+      case 8: {
+      }
+      break;
+      //Terminated
+      case 9: {
+      isTerminated = true;
+      }
+      break;
+      
+      default: {
+        print("Apologies, please re-enter an integer from the available options.");
+      }
+    }
+  }
+}
